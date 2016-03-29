78c901ee4e2aa3e3836573ecf7026002001cd029
sc545 <sc545545@naver.com>
2016-03-30 00:08:53 +0900

    scan device

---
+++ museum/app/src/main/java/com/example/testlib/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.testlib;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	
+}
